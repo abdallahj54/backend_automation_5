09a11796948fd9575efd59047cd6f0dbcf46b6e6
Abdallah Jaber <abdallahj708@gmail.com>
2023-03-12 09:44:32 -0500

    updated repo 3.11.23

--- src/test/java/api/tdd/go_rest/GoRestWithLombok.java
+++ src/test/java/api/tdd/go_rest/GoRestWithLombok.java
@@ -3,20 +3,31 @@ package api.tdd.go_rest;
 import api.pojo_classes.go_rest.CreateGoRestUserWithLombok;
 import api.pojo_classes.go_rest.UpdateGoRestUserWithLombok;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class GoRestWithLombok {
 
+    static Logger logger = LogManager.getLogger(GoRestWithLombok.class);
+
     Response response;
+    /**
+     * ObjectMapper is a class coming form fasterxml to convert Java object to Json
+     */
+    ObjectMapper objectMapper = new ObjectMapper();
 
     Faker faker = new Faker();
 
@@ -29,88 +40,130 @@ public class GoRestWithLombok {
     String expectedGoRestStatus;
 
     @BeforeTest
-    public void beforeTest(){
-        System.out.println("Starting the API Test");
-        /**
-         * By having RestAssured URI set implicitly into rest assured
-         * we just add a path to the post call
-         */
+    public void beforeTest() {
+        System.out.println("Starting the API test");
+        // By having RestAssured URI set implicitly in to rest assured
+        // we just add path to the post call
         RestAssured.baseURI = ConfigReader.getProperty("GoRestBaseURI");
     }
 
     @Test
     public void goRestCRUDWithLombok() throws JsonProcessingException {
-        // Creating a POJO(Bean) request body with lombok (like an object)
-        System.out.println("\n-----Creating the User-----\n");
+        // Creating a POJO (Bean) object
+
         CreateGoRestUserWithLombok createUser = CreateGoRestUserWithLombok
+                // with the help of the Lombok, we are assigning the values to variables
+                //coming from Bean class
                 .builder()
-                .name("Abdallah Jaber")
+                .name("Tech Global")
                 .email(faker.internet().emailAddress())
-                .gender("male")
+                .gender("female")
                 .status("active")
                 .build();
 
+        System.out.println("Creating the user");
         response = RestAssured
                 .given().log().all()
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
                 .header("Authorization", ConfigReader.getProperty("GoRestToken"))
                 .body(createUser)
+//                .when().post("https://gorest.co.in/public/v2/users")
                 .when().post("/public/v2/users")
                 .then().log().all()
+                //validating the status code with rest assured
                 .and().assertThat().statusCode(201)
-                .time(Matchers.lessThan(3000L))
-                .body("name", equalTo("Abdallah Jaber"))
+                //validating the response time is less than the specified one
+                //.time(Matchers.lessThan(3000L))
+                //validating the value from the body with hamcrest
+                .body("name", equalTo("Tech Global"))
+                //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
+
+        System.out.println("Fetching the user");
         goRestId = response.jsonPath().getInt("id");
 
-
-        System.out.println("\n-----Getting the User-----\n");
         response = RestAssured
                 .given().log().all()
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
                 .header("Authorization", ConfigReader.getProperty("GoRestToken"))
                 .when().get("/public/v2/users/" + goRestId)
                 .then().log().all()
+                //validating the status code with rest assured
                 .and().assertThat().statusCode(200)
-                .time(Matchers.lessThan(3000L))
-                .body("name", equalTo("Abdallah Jaber"))
+                //validating the response time is less than the specified one
+                //.time(Matchers.lessThan(3000L))
+                //validating the value from the body with hamcrest
+                .body("name", equalTo("Tech Global"))
+                //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
 
-        System.out.println("\n-----Updating the User-----\n");
-        UpdateGoRestUserWithLombok updateUser = UpdateGoRestUserWithLombok
+        System.out.println("Updating the user");
+        UpdateGoRestUserWithLombok updateGoRestUserWithLombok = UpdateGoRestUserWithLombok
+                //building the update java body
                 .builder()
                 .email(faker.internet().emailAddress())
-                .gender("female")
+                .gender("male")
                 .status("inactive")
                 .build();
 
         response = RestAssured
                 .given().log().all()
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
                 .header("Authorization", ConfigReader.getProperty("GoRestToken"))
-                .body(updateUser)
-                .when().patch("/public/v2/users/" + goRestId)
+                .body(updateGoRestUserWithLombok)
+                .when().put("/public/v2/users/" + goRestId)
                 .then().log().all()
+                //validating the status code with rest assured
                 .and().assertThat().statusCode(200)
-                .time(Matchers.lessThan(3000L))
-                .body("name", equalTo("Abdallah Jaber"))
+                //validating the response time is less than the specified one
+                //.time(Matchers.lessThan(3000L))
+                //validating the value from the body with hamcrest
+                .body("name", equalTo("Tech Global"))
+                //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
 
-        System.out.println("\n-----Deleting the User-----\n");
-        response = RestAssured
-                .given().log().all()
-                .contentType(ContentType.JSON)
-                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
-                .when().delete("/public/v2/users/" + goRestId)
-                .then().log().all()
-                .and().assertThat().statusCode(204)
-                .time(Matchers.lessThan(3000L))
-                .extract().response();
+        System.out.println("Deleting the user");
+//        /** -----------------------DELETE-----------------------*/
+//
+//        response = RestAssured
+//                .given().log().all()
+//                .contentType(ContentType.JSON)
+//                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
+//                .when().delete("/public/v2/users/" + goRestId)
+//                .then().log().all()
+//                .and().assertThat().statusCode(204)
+//                //validating the response time is less than the specified one
+//               // .time(Matchers.lessThan(3000L))
+//                .extract().response();
+
+
+        System.out.println("-----HamCrest-----");
+        // find expected name with LomBok
+        String expectedName = createUser.getName();
+
+        // find actual name with JayWay
+        String actualName = JsonPath.read(response.asString(), "name");
+
+        // debug wit logger
+        logger.debug("The name value should be " + actualName + " but we found " + expectedName);
+
+        // assert it with hamcrest
+        assertThat(
+                // the reason why we are asserting
+                "I am checking if expected name is matching with the actual name ",
+                // actual value
+                actualName,
+                // expected value
+                is(expectedName)
+        );
     }
 }
--- src/test/java/api/tdd/go_rest/GoRestWithoutLombok.java
+++ src/test/java/api/tdd/go_rest/GoRestWithoutLombok.java
@@ -1,29 +1,34 @@
 package api.tdd.go_rest;
 
 import api.pojo_classes.go_rest.CreateGoRestUserWithoutLombok;
+import api.pojo_classes.go_rest.UpdateGoRestUser;
 import api.pojo_classes.go_rest.UpdateGoRestUserWithoutLombok;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
+import com.jayway.jsonpath.JsonPath;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.hamcrest.Matchers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class GoRestWithoutLombok {
 
+    static Logger logger = LogManager.getLogger(GoRestWithoutLombok.class);
+
     Response response;
-
     /**
-     * ObjectMapper is a class coming from fasterxml to convert Java object to JSON
+     * ObjectMapper is a class coming form Jackson to convert a Java object to Json
      */
-
     ObjectMapper objectMapper = new ObjectMapper();
 
     Faker faker = new Faker();
@@ -35,90 +40,121 @@ public class GoRestWithoutLombok {
     String expectedGoRestStatus;
 
     @BeforeTest
-    public void beforeTest(){
-        System.out.println("Starting the API Test");
-        /**
-         * By having RestAssured URI set implicitly into rest assured
-         * we just add a path to the post call
-         */
+    public void beforeTest() {
+        System.out.println("Starting the API test");
+        // By having RestAssured URI set implicitly in to rest assured
+        // we just add path to the post call
         RestAssured.baseURI = ConfigReader.getProperty("GoRestBaseURI");
     }
 
     @Test
     public void goRestCRUD() throws JsonProcessingException {
-        // Creating a POJO(Bean) object
-        CreateGoRestUserWithoutLombok createGoRestUser = new CreateGoRestUserWithoutLombok();
+        // Creating a POJO (Bean) object
+        CreateGoRestUserWithoutLombok createGoRestUserWithoutLombok = new CreateGoRestUserWithoutLombok();
 
-        // Assigned the values to the attributes
-        createGoRestUser.setName("Abdallah Jaber");
-        createGoRestUser.setGender("male");
-        createGoRestUser.setEmail(faker.internet().emailAddress());
-        createGoRestUser.setStatus("active");
+        // assigned the values to the attributes
+        createGoRestUserWithoutLombok.setName("Tom");
+        createGoRestUserWithoutLombok.setGender("male");
+        createGoRestUserWithoutLombok.setEmail(faker.internet().emailAddress());
+        createGoRestUserWithoutLombok.setStatus("active");
 
-        System.out.println("-----Creating the user with POST Request-----\n");
+        System.out.println("======= Creating the user with POST request =========");
+
         response = RestAssured
                 .given().log().all()
-                //.header("Content-Type", "application/json")
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
-                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
-                .body(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(createGoRestUser))
-                //.when().post("https://gorest.co.in/public/v2/users")
+                .header("Authorization", "Bearer cd6f43f79e931dc381c5c228f3e80c9f6990ec23e970e0325e206b9d241e551e")
+                .body(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(createGoRestUserWithoutLombok))
+                // .when().post("https://gorest.co.in/public/v2/users")
                 .when().post("/public/v2/users")
                 .then().log().all()
-                // validating the status code with rest assured
+                //validating the status code with rest assured
                 .and().assertThat().statusCode(201)
-                // validating the response time is less than the specified amount of time
-                .time(Matchers.lessThan(10000L))
-                // validating the value from the body with hamcrest
-                .body("name", equalTo("Abdallah Jaber"))
-                // validating the response content type
+                //validating the response time is less than the specified one
+                //.time(Matchers.lessThan(4000L))
+                //validating the value of the body with hamcrest
+                .body("name", equalTo("Tom"))
+                //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
+        //expected status
+        String expectedStatus = createGoRestUserWithoutLombok.getStatus();
+        // actual status
+        String actualStatus = JsonPath.read(response.asString(), "status");
+        // Debug
+        logger.debug("The expected status is " + expectedStatus +  " but we found " + actualStatus);
+        // Hamcrest
+        assertThat(
+                // the reason why we are asserting
+                "I am checking if expected status is matching with the actual status ",
+                // actual value
+                actualStatus,
+                // expected value
+                is(expectedStatus)
+        );
 
-        System.out.println("\n-----Fetching the user with GET Request-----\n");
+
+
+
+
+
+
+        System.out.println("======= Fetching the user with GET request =========");
+
         expectedGoRestId = response.jsonPath().getInt("id");
 
         response = RestAssured
                 .given().log().all()
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
-                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
+                .header("Authorization", "Bearer cd6f43f79e931dc381c5c228f3e80c9f6990ec23e970e0325e206b9d241e551e")
                 .when().get("/public/v2/users/"+ expectedGoRestId)
                 .then().log().all()
                 //validating the status code with rest assured
                 .and().assertThat().statusCode(200)
                 //validating the response time is less than the specified one
-                .time(Matchers.lessThan(10000L))
+                // .time(Matchers.lessThan(4000L))
                 //validating the value from the body with hamcrest
-                .body("name", equalTo("Abdallah Jaber"))
+                .body("name", equalTo("Tom"))
                 //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
-        System.out.println("\n-----Updating the user with PUT request-----\n");
-        UpdateGoRestUserWithoutLombok updateGoRestUser = new UpdateGoRestUserWithoutLombok();
 
-        updateGoRestUser.setName("Michael Jordan");
+        System.out.println("-==========Updating a User with PUT request============-");
+
+
+        UpdateGoRestUserWithoutLombok updateGoRestUser = new UpdateGoRestUserWithoutLombok();
+        updateGoRestUser.setName("TGlobal");
         updateGoRestUser.setEmail(faker.internet().emailAddress());
 
         response = RestAssured
                 .given().log().all()
+//                .header("Content-Type", "application/json")
                 .contentType(ContentType.JSON)
-                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
+                .header("Authorization", "Bearer cd6f43f79e931dc381c5c228f3e80c9f6990ec23e970e0325e206b9d241e551e")
                 .body(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(updateGoRestUser))
-                .when().put("/public/v2/users/"+ expectedGoRestId)
+//                .when().post("https://gorest.co.in/public/v2/users")
+                .when().put("/public/v2/users/" + expectedGoRestId)
                 .then().log().all()
+                //validating the status code with rest assured
                 .and().assertThat().statusCode(200)
-                .time(Matchers.lessThan(10000L))
-                .body("name", equalTo("Michael Jordan"))
+                //validating the response time is less than the specified one
+                //.time(Matchers.lessThan(6000L))
+                //validating the value from the body with hamcrest
+                .body("name", equalTo("TGlobal"))
+                //validating the response content type
                 .contentType(ContentType.JSON)
                 .extract().response();
 
         expectedGoRestName = updateGoRestUser.getName();
         expectedGoRestEmail = updateGoRestUser.getEmail();
-        expectedGoRestGender = createGoRestUser.getGender();
-        expectedGoRestStatus = createGoRestUser.getStatus();
+        expectedGoRestGender = createGoRestUserWithoutLombok.getGender();
+        expectedGoRestStatus = createGoRestUserWithoutLombok.getStatus();
 
+        //"id" in the getInt is the name of the attribute in the response body
         int actualGoRestId = response.jsonPath().getInt("id");
         String actualGoRestName = response.jsonPath().getString("name");
         String actualGoRestEmail = response.jsonPath().getString("email");
@@ -131,15 +167,17 @@ public class GoRestWithoutLombok {
         Assert.assertEquals(actualGoRestGender, expectedGoRestGender);
         Assert.assertEquals(actualGoRestStatus, expectedGoRestStatus);
 
-        System.out.println("\n-----Deleting the user with DELETE Request------\n");
+        System.out.println("-==========Deleting a User with DELETE request============-");
+
         response = RestAssured
                 .given().log().all()
                 .contentType(ContentType.JSON)
-                .header("Authorization", ConfigReader.getProperty("GoRestToken"))
+                .header("Authorization","Bearer cd6f43f79e931dc381c5c228f3e80c9f6990ec23e970e0325e206b9d241e551e")
                 .when().delete("/public/v2/users/"+ expectedGoRestId)
                 .then().log().all()
                 .and().assertThat().statusCode(204)
-                .time(Matchers.lessThan(10000L))
+                //.time(Matchers.lessThan(6000L))
                 .extract().response();
+
     }
 }
